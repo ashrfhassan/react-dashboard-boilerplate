48aa829c26f25be92cf91f486d5eb8851a1a579c
ashraf hassan <ashrfhassan890@gmail.com>
2023-03-31 20:40:15 +0200

    added richbox component

---
+++ src/components/richBox/index.tsx
@@ -0,0 +1,62 @@
+import Styles from './index.module.scss';
+import React, { useEffect, useRef, useState } from 'react';
+import ClassicEditor from './package/ckeditor';
+import { AiOutlineExclamationCircle } from 'react-icons/ai';
+import { renderError } from '../../helpers/renderers';
+import Label from '../label';
+import i18n from '../../i18n';
+
+export type RichBoxProps = {
+  className?: string;
+  labelText?: string;
+  onChange: (content: any) => void;
+  errorMessage?: string | Array<string>;
+};
+
+const RichBox = React.forwardRef(
+  (
+    { className, labelText, onChange, errorMessage }: RichBoxProps,
+    ref: React.Ref<HTMLHeadingElement>
+  ) => {
+    const boxRef = useRef(null);
+    const [initEditor, setInitEditor] = useState<any>(null);
+    useEffect(() => {
+      initEditor?.destroy();
+      if (boxRef && initEditor === null) {
+        (ClassicEditor as any)
+          .create(boxRef.current as any, {
+            language: i18n.language,
+            autosave: {
+              save: (editor: any): any => {
+                onChange(editor.getData());
+              },
+            },
+          })
+          .then((editor: any) => {
+            setInitEditor(editor);
+          })
+          .catch((error: any) => {
+            console.error(
+              'There was a problem initializing the editor.',
+              error
+            );
+          });
+      }
+    }, [boxRef]);
+
+    return (
+      <>
+        {labelText ? (
+          <Label text={labelText} className={Styles['input-label']} />
+        ) : (
+          <></>
+        )}
+        <textarea ref={boxRef}></textarea>
+        {errorMessage && renderError(errorMessage)}
+      </>
+    );
+  }
+);
+
+RichBox.displayName = 'RichBox';
+export default RichBox;
